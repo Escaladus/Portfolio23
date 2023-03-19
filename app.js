58224144956a223765c65f5a01254f33c02fed5c
deske <escaladus@gmail,com>
2023-03-19 23:17:48 +0200

    lisätty chroma kirjasto, luminanssi

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ function generateRandomColor() {
 }
 function setRanfdomColors() {
     cols.forEach(col => {
+        const text = col.querySelector('h2')
+        //const color = chroma.random()
+        const color = generateRandomColor();
+        text.textContent = color
         col.style.background = generateRandomColor()
+        setTextColor(text, color)
     })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRanfdomColors()
