eab5b5b902e06f0c20db04220b1ac4eab1d2ed8e
deske <escaladus@gmail,com>
2023-03-19 23:52:04 +0200

    Loppuversio1

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const cols = document.querySelectorAll('.col')
+
+document.addEventListener('keydown', event => {
+    event.preventDefault()
+    if(event.code.toLowerCase() === 'space') {
+        setRanfdomColors()
+    }
+})
+
+document.addEventListener('click', event => {
+    const type = event.target.dataset.type
+    if(type === 'lock') {
+        const node = event.target.tagName.toLowerCase() === 'i'
+            // jos valittu elementti ei ole "i", valitaan kyseisen elementin lapsen
+            ? event.target
+            : event.target.children[0]
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+        copyToClipboard(event.target.textContent)
+    }
+})
 function generateRandomColor() {
     //RGB
     const hexCodes = '0123456789ABCDEF'
@@ -8,14 +29,23 @@ function generateRandomColor() {
     }
     return '#' + color
 }
+function copyToClipboard(text) {
+    return  navigator.clipboard.writeText(text)
+}
 function setRanfdomColors() {
     cols.forEach(col => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
+        const button = col.querySelector('button')
         //const color = chroma.random()
-        const color = generateRandomColor();
+        const color = generateRandomColor()
+        if(isLocked) {
+            return
+        }
         text.textContent = color
         col.style.background = generateRandomColor()
         setTextColor(text, color)
+        setTextColor(button, color)
     })
 }
 
